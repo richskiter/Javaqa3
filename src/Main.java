42e151eeef04824cd31b6eca7c8882ceb3ab73b2
Rishat Karimov <richskiter@gmail.com>
2021-01-08 16:31:11 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9999;
+        int threshold = 20;
+        int miles = ticket / threshold;
+        System.out.println(miles);
+    }
+}
